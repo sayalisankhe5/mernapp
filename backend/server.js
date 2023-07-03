e7221b970da87704bc989aa98effe388a76851d9
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-07-03 09:16:13 +0530

    first commit backend

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3500;
+const path = require("path");
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use("/", require("./routes/root"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "NotFound.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "Sorry, Not found" });
+  } else {
+    res.type("txt").send("Not found ");
+  }
+});
+
+app.listen(PORT, () => console.log(`server is listening to ${PORT}`));
