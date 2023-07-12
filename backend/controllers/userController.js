aa94cd48ecfe7b255a7415a2c9bacd61010dd845
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-07-12 09:39:46 +0530

    new user method updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
 const getAllUsers = asyncHandler(async (req, res) => {
@@ -9,7 +10,26 @@ const getAllUsers = asyncHandler(async (req, res) => {
   res.json(users);
 });
 
-const createNewUser = asyncHandler(async (req, res) => {});
+const createNewUser = asyncHandler(async (req, res) => {
+  const { username, password, roles } = req.body;
+
+  if (!username || !password || !Array.isArray(roles) || !roles.length) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  const duplicate = (await User.find({ username }).lean()).exec();
+  if (duplicate) {
+    return res.status(409).json({ message: "duplicate user present" });
+  }
+
+  const hashedPswd = await bcrypt(password, 10);
+
+  const user = await User.create({ username, password: hashedPswd, roles });
+  if (user) {
+    res.status(201).json({ message: `New user ${username} is created` });
+  } else {
+    res.status(400).json({ message: "Invalid data" });
+  }
+});
 
 const updateUser = asyncHandler(async (req, res) => {});
 
