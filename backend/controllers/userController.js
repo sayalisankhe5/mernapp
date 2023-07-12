3dff9d956c119e162c10fb02b95497ec2347a2a5
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-07-12 09:26:51 +0530

    get users method updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,13 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/User");
 
-const getAllUsers = asyncHandler(async (req, res) => {});
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find().select("-password").lean();
+  if (!users) {
+    return res.status(400).json({ message: "Users data not found" });
+  }
+  res.json(users);
+});
 
 const createNewUser = asyncHandler(async (req, res) => {});
 
