f091e3cd1b3f8e984130ce21e1fc1a6e8ee050e2
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-07-15 20:16:54 +0530

    Update noteController.js

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -1,8 +1,34 @@
 const asyncHandler = require("express-async-handler");
 const Note = require("../models/Note");
+const User = require("../models/User");
 
-const getAllNotes = asyncHandler(async (req, res) => {});
-const createNote = asyncHandler(async (req, res) => {});
+const getAllNotes = asyncHandler(async (req, res) => {
+  const notes = await Note.find().lean();
+
+  if (!notes?.length) {
+    return res.status(400).json({ message: "No notes found" });
+  }
+
+  res.json(notes);
+});
+const createNote = asyncHandler(async (req, res) => {
+  const { user, title, text } = req.body;
+  if (!user || !title || !text) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  //   const existingUser = await User.findById({ _id: user }).lean().exec();
+  //   if (!existingUser) {
+  //     return res.status(400).json({ message: "No such user found" });
+  //   }
+
+  const note = await Note.create({ user, title, text });
+  if (!note) {
+    return res.status(201).json({ message: "new note is created" });
+  } else {
+    return res.status(400).json({ message: "Invalid data" });
+  }
+});
 const updateNote = asyncHandler(async (req, res) => {});
 const deleteNote = asyncHandler(async (req, res) => {});
 
