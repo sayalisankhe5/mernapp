3c0c00c9065ab5644077d225d8dd12d51d51e1ea
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-07-18 09:04:59 +0530

    Update noteController.js

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -22,28 +22,45 @@ const createNote = asyncHandler(async (req, res) => {
     return res.status(400).json({ message: "No such user found" });
   }
 
+  const duplicateNote = await Note.findOne({ title }).lean().exec();
+  if (duplicateNote) {
+    return res.status(400).json({ message: "duplicate note found" });
+  }
+
   const note = await Note.create({ user, title, text });
-  if (!note) {
+  if (note) {
     return res.status(201).json({ message: "new note is created" });
   } else {
     return res.status(400).json({ message: "Invalid data" });
   }
 });
 const updateNote = asyncHandler(async (req, res) => {
-const {id,user,title,text,completed} = req.body;
+  const { id, user, title, text, completed } = req.body;
 
-if(!id || !user || !title || !text || typeof completed != "boolean"){
-  return res.status(400).json({message:"All fields are required "});
+  if (!id || !user || !title || !text || typeof completed != "boolean") {
+    return res.status(400).json({ message: "All fields are required " });
+  }
+  const note = await Note.findById(id).exec();
 
-const note = await Note.findById(id).exec();
+  if (!note) {
+    return res.status(400).json({ message: "No such note found" });
+  }
 
-if(!note){
-  return res.status(400).json({message:"No such note found"})
-}
+  const existingUser = await User.findById(user).lean().exec();
+  if (!existingUser) {
+    return res.status(400).json({ message: "no such user found" });
+  }
+  const duplicateNote = await Note.findOne({ title }).lean().exec();
+  if (duplicateNote && duplicateNote?._id.toString() != id) {
+    return res.status(409).json({ message: "duplicate note found" });
+  }
 
-}
-
-
+  note.user = user;
+  note.title = title;
+  note.text = text;
+  note.completed = completed;
+  const updatedNote = await note.save();
+  res.json({ message: `note with title ${updatedNote.title} is updated` });
 });
 const deleteNote = asyncHandler(async (req, res) => {
   const { id } = req.body;
