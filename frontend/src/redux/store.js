ed764bd30915961a267ede9c78993039934ff985
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-07-25 08:47:59 +0530

    state management

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,7 +1,12 @@
+import apiSlice from "./api/apiSlice";
+
 const { configureStore } = require("@reduxjs/toolkit");
 
 const store = configureStore({
-  reducer: {},
+  reducer: { [apiSlice.reducerPath]: apiSlice.reducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
 
 export default store;
