182564b2e7f1a1cec01dc53ee36c9aa9e02e9e8a
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-07-25 09:38:18 +0530

    Update usersApiSlice.js

--- frontend/src/components/Users/usersApiSlice.js
+++ frontend/src/components/Users/usersApiSlice.js
@@ -12,8 +12,27 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         return response.status === 200 && !result.isError;
       },
       keepUnusedDataFor: 5,
-      transformResponse: (responseData) => {},
-      providesTags: (result, error, arg) => {},
+      transformResponse: (responseData) => {
+        let loadedUsers = responseData.map((user) => {
+          user.id = user._id;
+          return user;
+        });
+
+        return usersAdapter.setAll(initialState, loadedUsers);
+      },
+      providesTags: (result, error, arg) => {
+        if (result?.ids) {
+          return [
+            { type: "User", id: "LIST" },
+            ...result.ids.map((id) => ({
+              type: "User",
+              id,
+            })),
+          ];
+        } else {
+          return [{ type: "User", id: "LIST" }];
+        }
+      },
     }),
   }),
 });
