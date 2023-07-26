aac771483504dbcec0f80094c33a32929e01684b
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-07-26 09:25:17 +0530

    Update notesApiSlice.js

--- frontend/src/components/Notes/notesApiSlice.js
+++ frontend/src/components/Notes/notesApiSlice.js
@@ -12,6 +12,27 @@ export const notesApiSlice = apiSlice.injectEndpoints({
       validateStatus: (response, result) => {
         return response.status === 200 && !result.isError;
       },
+      keepUnusedDataFor: 5,
+      transformResponse: (responseData) => {
+        let loadedNotes = responseData.map((note) => {
+          note.id = note._id;
+          return note;
+        });
+        return notesAdapter.setAll(initialState, loadedNotes);
+      },
+      providesTags: (result, error, arg) => {
+        if (result?.ids) {
+          return [
+            { type: "Notes", id: "LIST" },
+            ...result.ids.map((id) => ({
+              type: "Notes",
+              id,
+            })),
+          ];
+        } else {
+          return [{ type: "Notes", id: "LIST" }];
+        }
+      },
     }),
   }),
 });
