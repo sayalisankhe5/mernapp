5770a511aa1f16b37badd3329a47e68ff30f0a79
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-07-27 09:29:38 +0530

    Update User.js

--- frontend/src/components/Users/User.js
+++ frontend/src/components/Users/User.js
@@ -1,5 +1,21 @@
-const User = () => {
-  return <></>;
+import { useSelector } from "react-redux";
+import { selectUserById } from "./usersApiSlice";
+import { useNavigate } from "react-router-dom";
+
+const User = (props) => {
+  const user = useSelector((state) => selectUserById(state, props.userId));
+  const navigate = useNavigate();
+  if (user) {
+    const handleEdit = () => navigate(`/dash/users/${props.userId}`);
+    const userRolesString = user.roles.toString().Replace(",", ", ");
+    return (
+      <tr>
+        <td>{user.username}</td>
+        <td>{userRolesString}</td>
+        <td onClick={handleEdit}>Edit</td>
+      </tr>
+    );
+  } else return null;
 };
 
 export default User;
