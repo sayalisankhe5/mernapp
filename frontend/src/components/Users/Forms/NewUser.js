b1ae18723c3bb40e1a83687bd46422cf233eb3d7
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-01 08:52:49 +0530

    Update NewUser.js

--- frontend/src/components/Users/Forms/NewUser.js
+++ frontend/src/components/Users/Forms/NewUser.js
@@ -1,5 +1,129 @@
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+import { useAddNewUserMutation } from "../usersApiSlice";
+import { ROLES } from "../../../config/roles";
 const NewUser = () => {
-  return <></>;
+  const USERNAME_REGEX = /^[A-z]{3-20}$/;
+  const PSWD_REGEX = /^[A-z0-9!@#$%]{4-12}$/;
+
+  const [username, setUsername] = useState("");
+  const [validUsername, setValidUsername] = useState(false);
+  const [password, setPassword] = useState("");
+  const [validPassword, setValidPassword] = useState(false);
+  const [roles, setRoles] = useState(["Employee"]);
+  const navigate = useNavigate();
+  const [addNewUser, { isLoading, isSuccess, isError, error }] =
+    useAddNewUserMutation();
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const canSave =
+    [roles.length, validPassword, validUsername].every(Boolean) && !isLoading;
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    if (canSave) {
+      await addNewUser({ username, password, roles });
+    }
+  };
+  const options = Object.values(ROLES).map((r) => {
+    return (
+      <option key={r} value={r}>
+        {r}
+      </option>
+    );
+  });
+
+  const onRolesChanged = (e) => {
+    const values = Array.from(e.target.selectedOptions, (option) => {
+      return option.value;
+    });
+
+    setRoles(values);
+  };
+
+  useEffect(() => {
+    setValidUsername(USERNAME_REGEX.test(username));
+  }, [username]);
+  useEffect(() => {
+    setValidPassword(PSWD_REGEX.test(password));
+  }, [password]);
+  useEffect(() => {
+    if (isSuccess) {
+      setUsername("");
+      setPassword("");
+      setRoles([]);
+      navigate("/dash/users");
+    }
+  }, [isSuccess]);
+
+  const errClass = isError ? "errmsg" : "offscreen";
+  const validUserClass = !validUsername ? "form__input--incomplete" : "";
+  const validPwdClass = !validPassword ? "form__input--incomplete" : "";
+  const validRolesClass = !Boolean(roles.length)
+    ? "form__input--incomplete"
+    : "";
+
+  return (
+    <>
+      <p className={errClass}>{error?.data?.message}</p>
+
+      <form className="form" onSubmit={onSubmitHandler}>
+        <div className="form__title-row">
+          <h2>New User</h2>
+          <div className="form__action-buttons">
+            <button className="icon-button" title="Save" disabled={!canSave}>
+              <FontAwesomeIcon icon={faSave} />
+            </button>
+          </div>
+        </div>
+        <label className="form__label" htmlFor="username">
+          Username: <span className="nowrap">[3-20 letters]</span>
+        </label>
+        <input
+          className={`form__input ${validUserClass}`}
+          id="username"
+          name="username"
+          type="text"
+          autoComplete="off"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+
+        <label className="form__label" htmlFor="password">
+          Password: <span className="nowrap">[4-12 chars incl. !@#$%]</span>
+        </label>
+        <input
+          className={`form__input ${validPwdClass}`}
+          id="password"
+          name="password"
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+
+        <label className="form__label" htmlFor="roles">
+          ASSIGNED ROLES:
+        </label>
+        <select
+          id="roles"
+          name="roles"
+          className={`form__select ${validRolesClass}`}
+          multiple={true}
+          size="3"
+          value={roles}
+          onChange={onRolesChanged}
+        >
+          {options}
+        </select>
+      </form>
+    </>
+  );
 };
 
 export default NewUser;
