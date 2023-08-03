e784281a21c836cfe8c833f08848e1f565b5a365
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-03 08:47:08 +0530

    Update WelcomeUser.js

--- frontend/src/components/Auth/WelcomeUser.js
+++ frontend/src/components/Auth/WelcomeUser.js
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+
 const WelcomeUser = () => {
-  return <h2>Hello user</h2>;
+  return (
+    <section className="welcome">
+      <h2>Hello user</h2>
+      <p>
+        <Link to="/dash/notes">View All Notes</Link>
+      </p>
+      <p>
+        <Link to="/dash/users">View All Users</Link>
+      </p>
+      <p>
+        <Link to="/dash/notes/new">Add new Note</Link>
+      </p>
+      <p>
+        <Link to="/dash/users/new">Add new User</Link>
+      </p>
+    </section>
+  );
 };
 
 export default WelcomeUser;
