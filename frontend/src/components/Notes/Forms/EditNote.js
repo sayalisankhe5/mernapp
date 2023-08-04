a87fc1a2817d77a9835c232f4ec1ffb1d5fab25c
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-04 13:45:30 +0530

    Update EditNote.js

--- frontend/src/components/Notes/Forms/EditNote.js
+++ frontend/src/components/Notes/Forms/EditNote.js
@@ -1,5 +1,20 @@
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { selectAllUsers } from "../../Users/usersApiSlice";
+import { selectNoteById } from "../notesApiSlice";
+import EditNoteForm from "./EditNoteForm";
+
 const EditNote = () => {
-  return <></>;
+  const { id } = useParams();
+  const users = useSelector(selectAllUsers);
+  const note = useSelector((state) => selectNoteById(state, id));
+  const content =
+    note && users ? (
+      <EditNoteForm note={note} users={users} />
+    ) : (
+      <p>Loading...</p>
+    );
+  return content;
 };
 
 export default EditNote;
