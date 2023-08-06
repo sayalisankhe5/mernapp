c23892493b52b652ddf1a7392c06e195d7db42d7
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-06 19:29:32 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const loginLimiter = require("../middleware/loginLimiter");
 const router = express.Router();
 
-router.route("/").post();
+router.route("/").post(loginLimiter);
 router.route("/refresh").get();
 router.route("/logout").post();
 
