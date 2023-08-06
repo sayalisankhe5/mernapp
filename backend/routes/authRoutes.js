d46701c54725403394256c104e17200d85224666
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-06 19:27:54 +0530

    Create authRoutes.js

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.route("/").post();
+router.route("/refresh").get();
+router.route("/logout").post();
+
+module.exports = router;
