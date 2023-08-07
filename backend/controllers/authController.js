e3c44ff856911391eeed10730b3f0320bfac69f2
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-07 08:43:03 +0530

    Create authController.js

---
+++ backend/controllers/authController.js
@@ -0,0 +1,7 @@
+const asyncHandler = require("express-async-handler");
+
+const login = asyncHandler((req, res) => {});
+const refresh = (req, res) => {};
+const logout = (req, res) => {};
+
+module.exports = { login, refresh, logout };
