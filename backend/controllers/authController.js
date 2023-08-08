715f72353c5bd7e488fbc6defd9a6db8d9fc8483
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-08 08:30:00 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,83 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const JWT = require("jsonwebtoken");
 
-const login = asyncHandler((req, res) => {});
-const refresh = (req, res) => {};
-const logout = (req, res) => {};
+const login = asyncHandler(async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  const foundUser = await User.findOne({ username }).exec();
+  if (!foundUser || !foundUser.active) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const match = await bcrypt.compare(password, foundUser.password);
+  if (!match) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const accessToken = JWT.sign(
+    { UserInfo: { username: foundUser.username, roles: foundUser.roles } },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "1m",
+    }
+  );
+
+  const refreshToken = JWT.sign(
+    { username: foundUser.username },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: "1d",
+    }
+  );
+
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
+  res.json({ accessToken });
+});
+const refresh = (req, res) => {
+  const cookies = req.cookies;
+  if (!cookies?.jwt) {
+    return res.status(403).json({ message: "no cookie" });
+  }
+  const refreshToken = cookies.jwt;
+  JWT.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
+    asyncHandler(async (err, decoded) => {
+      if (err) {
+        return res.status(403).json({ message: "Forbidden" });
+      }
+      const foundUser = await User.findOne({
+        username: decoded.username,
+      }).exec();
+      if (!foundUser || !foundUser.active) {
+        return res.status(403).json("No such user found");
+      }
+      const accessToken = JWT.sign(
+        { UserInfo: { username: foundUser.username, roles: foundUser.roles } },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1m" }
+      );
+
+      res.json({ accessToken });
+    })
+  );
+};
+const logout = (req, res) => {
+  const cookies = req.cookies;
+  if (!cookies?.jwt) {
+    return res.sendStatus(204);
+  }
+  res.clearCookie("jwt", { httpOnly: true, secure: true, sameSite: "none" });
+  res.json({ message: "cookie is cleared" });
+};
 
 module.exports = { login, refresh, logout };
