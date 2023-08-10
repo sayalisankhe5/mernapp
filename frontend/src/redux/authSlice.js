3cbe17eaa277fe403a4ae8c5d49ab281487348f9
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-10 08:43:08 +0530

    Update authSlice.js

--- frontend/src/redux/authSlice.js
+++ frontend/src/redux/authSlice.js
@@ -2,4 +2,16 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
   name: "auth",
+  initialState: { token: null },
+  reducers: {
+    setCredentials: (state, action) => {
+      state.token = action.payload.accessToken;
+    },
+    logout: (state, action) => {
+      state.token = null;
+    },
+  },
 });
+
+export const { setAuthToken, logout } = authSlice.actions;
+export default authSlice.reducer;
