ae591656b75319369a3e001cf2fff4aeb17401f3
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-11 08:49:11 +0530

    Update Login.js

--- frontend/src/components/Auth/Login.js
+++ frontend/src/components/Auth/Login.js
@@ -1,5 +1,55 @@
+import { useRef, useState } from "react";
+import { useLoginMutation } from "./authApiSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setCredentials } from "../../redux/authSlice";
+
 const Login = () => {
-  return <h2>Login</h2>;
+  const userRef = useRef();
+  const errRef = useRef();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [login, { isLoading }] = useLoginMutation();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const accessToken = await login({ username, password }).unwrap();
+    dispatch(setCredentials({ accessToken }));
+  };
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  return (
+    <section>
+      <header></header>
+      <main></main>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          className=""
+          id="username"
+          type="text"
+          value={username}
+          onChange={handleUsernameChange}
+          required
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          className=""
+          id="password"
+          value={password}
+          type="password"
+          onChange={handlePasswordChange}
+          required
+        />
+      </form>
+    </section>
+  );
 };
 
 export default Login;
