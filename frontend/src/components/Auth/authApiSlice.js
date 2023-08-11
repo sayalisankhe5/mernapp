191700f4c1323314a86daf9169aef28a1c5feafd
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-11 08:48:39 +0530

    Update authApiSlice.js

---
+++ frontend/src/components/Auth/authApiSlice.js
@@ -0,0 +1,35 @@
+import apiSlice from "../../redux/api/apiSlice";
+import { logout } from "../../redux/authSlice";
+
+const authApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: "/auth",
+        method: "POST",
+        body: { ...credentials },
+      }),
+    }),
+    sendLogout: builder.mutation({
+      query: () => ({
+        url: "/auth/logout",
+        method: "POST",
+      }),
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        try {
+          await queryFulfilled;
+          dispatch(logout());
+          dispatch(apiSlice.util.resetApiState());
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
+    refresh: builder.mutation({
+      query: () => ({ url: "/auth/refresh", method: "GET" }),
+    }),
+  }),
+});
+
+export const { useLoginMutation, useSendLogoutMutation, useRefreshMutation } =
+  authApiSlice;
