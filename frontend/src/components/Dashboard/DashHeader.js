6102716176bf7631a0315f5bc5a3c137ef678102
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-12 08:29:11 +0530

    Update DashHeader.js

--- frontend/src/components/Dashboard/DashHeader.js
+++ frontend/src/components/Dashboard/DashHeader.js
@@ -1,13 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useSendLogoutMutation } from "../Auth/authApiSlice";
+import { useEffect } from "react";
 
 const DashHeader = () => {
+  const [sendLogout, { isLoading, isSuccess, isError, error }] =
+    useSendLogoutMutation();
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  let dashClass = "";
+  let DASH_REGEX = /^\/dash(\/)$/;
+  let USERS_REGEX = /^\/dash\/users(\/)$/;
+  let NOTES_REGEX = /^\/dash\/notes(\/)$/;
+  useEffect(() => {
+    if (isSuccess) {
+      navigate("/");
+    }
+  }, [isSuccess]);
+  if (isLoading) return <p>Logging out...</p>;
+  if (isError) return <p>Error: {error?.data?.message}</p>;
   return (
     <header className="dash-header">
-      <div className="dash-header__container">
+      <div className={`dash-header__container${dashClass}`}>
         <Link to="/dash">
           <h1 className="dash-header__title">techNotes</h1>
         </Link>
-        <nav className="dash-header__nav">{/* add nav buttons later */}</nav>
+        <nav className="dash-header__nav">
+          {/* add nav buttons later */}
+          <button onClick={() => sendLogout()}></button>
+        </nav>
       </div>
     </header>
   );
