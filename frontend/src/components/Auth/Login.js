9bf4103f1cdb35e04bd2186a4781132aa9b89ec1
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-12 08:29:01 +0530

    Update Login.js

--- frontend/src/components/Auth/Login.js
+++ frontend/src/components/Auth/Login.js
@@ -1,7 +1,7 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useLoginMutation } from "./authApiSlice";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { setCredentials } from "../../redux/authSlice";
 
 const Login = () => {
@@ -13,11 +13,35 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [login, { isLoading }] = useLoginMutation();
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+  useEffect(() => {
+    setError("");
+  }, [username, password]);
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const accessToken = await login({ username, password }).unwrap();
-    dispatch(setCredentials({ accessToken }));
+    try {
+      const { accessToken } = await login({ username, password }).unwrap();
+      dispatch(setCredentials({ accessToken }));
+      setUsername("");
+      setPassword("");
+      navigate("/dash");
+    } catch (err) {
+      if (!err.status) {
+        setError("No Server Response");
+      } else if (err.status === 400) {
+        setError("Missing Username or Password");
+      } else if (err.status === 401) {
+        setError("Unauthorized");
+      } else {
+        setError(err.data?.message);
+      }
+
+      errRef.current.focus();
+    }
   };
+  const errClass = error ? "errmsg" : "offscreen";
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
   };
@@ -25,31 +49,47 @@ const Login = () => {
     setPassword(e.target.value);
   };
   return (
-    <section>
-      <header></header>
-      <main></main>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username</label>
-        <input
-          className=""
-          id="username"
-          type="text"
-          value={username}
-          onChange={handleUsernameChange}
-          required
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          className=""
-          id="password"
-          value={password}
-          type="password"
-          onChange={handlePasswordChange}
-          required
-        />
-        <button>Sign In</button>
-      </form>
-    </section>
+    <>
+      {isLoading ? (
+        <p>Loading....</p>
+      ) : (
+        <section className="public">
+          <header>
+            <h1>Employee Login</h1>
+          </header>
+          <main className="login">
+            <p ref={errRef} className={errClass} aria-live="assertive">
+              {error}
+            </p>
+            <form className="form" onSubmit={handleSubmit}>
+              <label htmlFor="username">Username</label>
+              <input
+                className="form__input"
+                id="username"
+                type="text"
+                value={username}
+                onChange={handleUsernameChange}
+                required
+                ref={userRef}
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                className="form__input"
+                id="password"
+                value={password}
+                type="password"
+                onChange={handlePasswordChange}
+                required
+              />
+              <button className="form__submit-button">Sign In</button>
+            </form>
+          </main>
+          <footer>
+            <Link to="/">Back to home</Link>
+          </footer>
+        </section>
+      )}
+    </>
   );
 };
 
