5fe60d65ea782eb733b987bcaff19d5fc84d171b
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-13 16:36:28 +0530

    Update apiSlice.js

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,5 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:3500",
+  credentials: "include",
+  prepareHeaders: () => {},
+});
+
 const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
   tagTypes: ["Note", "User"],
