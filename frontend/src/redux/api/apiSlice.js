d8d6264efc846e054dc869d11bb44d0c500646de
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-14 17:09:15 +0530

    Update apiSlice.js

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,13 +1,38 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { setCredentials } from "../authSlice";
 
-const baseQuery = fetchBaseQuery({
+const baseQuery1 = fetchBaseQuery({
   baseUrl: "http://localhost:3500",
   credentials: "include",
-  prepareHeaders: () => {},
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token;
+
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+
+    return headers;
+  },
 });
 
+const baseQueryWithReAuth = async (args, api, extraOptions) => {
+  let result = await baseQuery1(args, api, extraOptions);
+  if (result?.error?.status == 403) {
+    const refreshResult = await baseQuery1("/auth/refresh", api, extraOptions);
+    if (refreshResult?.data) {
+      api.dispatch(setCredentials({ ...refreshResult.data }));
+      result = await baseQuery1(args, api, extraOptions);
+    } else if (refreshResult?.error?.status == 403) {
+      refreshResult.error.data.message = "login has expired";
+      return refreshResult;
+    }
+  }
+
+  return result;
+};
+
 const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
+  baseQuery: baseQueryWithReAuth,
   tagTypes: ["Note", "User"],
   endpoints: (builder) => ({}),
 });
