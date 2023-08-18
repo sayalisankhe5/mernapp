aafdb7e9d23043facf17a709100a68543101e927
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-18 09:05:12 +0530

    Update DashFooter.js

--- frontend/src/components/Dashboard/DashFooter.js
+++ frontend/src/components/Dashboard/DashFooter.js
@@ -1,9 +1,11 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useAuth from "../../hooks/useAuth";
 
 const DashFooter = () => {
   const navigate = useNavigate();
+  const { username, status } = useAuth();
   const { pathname } = useLocation();
   const goHomeClicked = () => navigate("/dash");
   let goHomeButton = null;
@@ -18,8 +20,8 @@ const DashFooter = () => {
   const content = (
     <footer className="dash-footer">
       {goHomeButton}
-      <p>Current User:</p>
-      <p>Status:</p>
+      <p>Current User:{username}</p>
+      <p>Status:{status}</p>
     </footer>
   );
 
