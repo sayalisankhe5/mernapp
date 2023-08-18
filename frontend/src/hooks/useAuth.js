bbe60b215b8bf9d615b462d5ec292ceecff6e694
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-18 09:04:48 +0530

    Create useAuth.js

---
+++ frontend/src/hooks/useAuth.js
@@ -0,0 +1,23 @@
+import { useSelector } from "react-redux";
+import jwtDecode from "jwt-decode";
+
+const useAuth = () => {
+  const token = useSelector((_state) => _state.auth.token);
+  let isManager = false;
+  let isAdmin = false;
+  let status = "Employee";
+
+  if (token) {
+    const decoded = jwtDecode(token);
+    const { username, roles } = decoded.UserInfo;
+    isManager = roles.includes("Manager");
+    isAdmin = roles.includes("Admin");
+    if (isManager) status = "Manager";
+    if (isAdmin) status = "Admin";
+    return { username, roles, isManager, isAdmin, status };
+  }
+
+  return { username: "", roles: [], isManager, isAdmin, status };
+};
+
+export default useAuth;
