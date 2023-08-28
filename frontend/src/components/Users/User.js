df239c910b39c29353faa75543107dbf44a64899
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-28 16:48:39 +0530

    Update User.js

--- frontend/src/components/Users/User.js
+++ frontend/src/components/Users/User.js
@@ -3,11 +3,13 @@ import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 
 import { useSelector } from "react-redux";
-import { selectUserById } from "./usersApiSlice";
+import { selectUserById, useGetUsersQuery } from "./usersApiSlice";
+import { memo } from "react";
 
 const User = ({ userId }) => {
-  const user = useSelector((state) => selectUserById(state, userId));
-
+  const { user } = useGetUsersQuery("usersList", {
+    selectFromResult: ({ data }) => ({ user: data?.entities[userId] }),
+  });
   const navigate = useNavigate();
 
   if (user) {
@@ -30,4 +32,5 @@ const User = ({ userId }) => {
     );
   } else return null;
 };
-export default User;
+
+export default memo(User);
