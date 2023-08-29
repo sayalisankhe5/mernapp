51cf5734a0d9eda8a82c7bd2107c97d8f1035d94
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-29 09:13:13 +0530

    Update EditUser.js

--- frontend/src/components/Users/Forms/EditUser.js
+++ frontend/src/components/Users/Forms/EditUser.js
@@ -1,13 +1,22 @@
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { selectUserById } from "../usersApiSlice";
+import { selectUserById, useGetUsersQuery } from "../usersApiSlice";
 import EditUserForm from "./EditUserForm";
+import PulseLoader from "react-spinners/PulseLoader";
 
 const EditUser = () => {
   const { id } = useParams();
-  const user = useSelector((state) => selectUserById(state, id));
+  //const user = useSelector((state) => selectUserById(state, id));
 
-  return <>{user ? <EditUserForm user={user} /> : <p>Loading....</p>}</>;
+  const { user } = useGetUsersQuery("usersList", {
+    selectFromResult: ({ data }) => ({
+      user: data?.entities[id],
+    }),
+  });
+
+  return (
+    <>{user ? <EditUserForm user={user} /> : <PulseLoader color={"#FFF"} />}</>
+  );
 };
 
 export default EditUser;
